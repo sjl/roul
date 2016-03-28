e63950bc087da0838c47db79669ca38f3ab3fe72
Steve Losh <steve@stevelosh.com>
2016-03-28 17:55:56 +0000

    Fix publish script

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ./build
-~/lib/virtualenvs/d/bin/d
+~/.virtualenvs/d/bin/d
 hg -R ~/src/sjl.bitbucket.org pull -u
 rsync --delete -a ./build/ ~/src/sjl.bitbucket.org/roul
 hg -R ~/src/sjl.bitbucket.org commit -Am 'roul: Update site.'
